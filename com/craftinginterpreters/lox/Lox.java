788230ff8b684cbb4968847779b97fff07816850
ArdaUsta13 <ardausta12@gmail.com>
2025-02-16 20:31:54 +0300

    Add Token class and TokenType enum

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -1,69 +1,68 @@
 package com.craftinginterpreters.lox;
 
-import java.io.BufferedReader;
+import Java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
-  static boolean hadError = false;
-  public static void main(String[] args) throws IOException {
-    if (args.length > 1) {
-      System.out.println("Usage: jlox [script]");
-      System.exit(64); // The number 64 comes from UNIX header that defines some conventions.
-    } else if (args.length == 1) { // For CLI usage.
-      runFile(args[0]);
-    } else {
-      runPrompt(); // For REPL usage.
+    static boolean hadError = false;
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usaage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
     }
-  }
 
-  // for normal interpretation like python test.py
-  private static void runFile(String path) throws IOException {
-    byte[] bytes = Files.readAllBytes(Paths.get(path));
-    run(new String(bytes, Charset.defaultCharset()));
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
 
-    // Indicate an error in the exit code.
-    if (hadError) System.exit(65);
-  }
-
-  // REPL itself
-  private static void runPrompt() throws IOException {  
-    InputStreamReader input = new InputStreamReader(System.in);
-    BufferedReader reader = new BufferedReader(input);
-
-    for (;;) { 
-      System.out.print("> ");
-      String line = reader.readLine();
-      if (line == null) break;
-      run(line);
-      hadError = false;
+        if (hadError) System.exit(65);
     }
-  }
 
-  private static void run(String source) {
-    Scanner scanner = new Scanner(source);
-    List<Token> tokens = scanner.scanTokens();
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        java.io.BufferedReader reader = new BufferedReader(input);
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break; // control-D returns null to reader.readLine()
+            run(line);
+
+            // if user makes a mistake, it shouldn't kill their whole session.
+            hadError = false;
+        }
     }
-  }
 
-  static void error(int line, String message) {
-    report(line, "", message);
-  }
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
 
-  private static void report(int line, String where,
-                             String message) {
-    System.err.println(
-        "[line " + line + "] Error" + where + ": " + message);
-    hadError = true;
-  }
-  
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    // Error Handling
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
 }
--- com/craftinginterpreters/lox/Token.java
+++ com/craftinginterpreters/lox/Token.java
@@ -1,21 +1,19 @@
 package com.craftinginterpreters.lox;
 
 class Token {
-  final TokenType type;
-  final String lexeme;
-  final Object literal;
-  final int line;
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+    
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
 
-  // TOKEN'S CONSTRUCTOR
-  Token(TokenType type, String lexeme, Object literal, int line) {
-    this.type = type;
-    this.lexeme = lexeme;
-    this.literal = literal;
-    this.line = line;
-  }
-
-  // GUESS WE GONNA USE @OVERRİDE HERE SOON ?
-  public String toString() {
-    return type + " " + lexeme + " " + literal;
-  }
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
 }
